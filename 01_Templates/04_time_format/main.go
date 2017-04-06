67b27b7c5229ed825beb96a11189dc1319d0588a
turdfurgeson <jtb2125@gmail.com>
2017-04-06 13:09:56 -0700

    fixes

--- 01_Templates/04_time_format/main.go
+++ 01_Templates/04_time_format/main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"html/template"
+	"log"
+	"os"
 	"time"
 )
 
 var tpl *template.Template
 
 func init() {
-	tpl := template.Must(template.ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
 }
 
 func monthDayYear(t time.Time) string {
 	return t.Format("01 - 02 - 2006")
 }
 
-func main() {
+var fm = template.FuncMap{
+	"fdateMDY": monthDayYear,
+}
 
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
