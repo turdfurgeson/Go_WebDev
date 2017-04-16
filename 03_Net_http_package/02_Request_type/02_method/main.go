eef3c2cc715adda50d340519e812c2a51a1be9d8
turdfurgeson <jtb2125@gmail.com>
2017-04-15 17:03:09 -0700

    Still going...

--- 03_Net_http_package/02_Request_type/02_method/main.go
+++ 03_Net_http_package/02_Request_type/02_method/main.go
@@ -1,1 +1,41 @@
-package _2_method
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"log"
+	"net/url"
+)
+
+var tpl *template.Template
+
+func init () {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+type airplane int
+
+func (a airplane) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	err := req.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method string
+		URL *url.URL
+		Submissions map[string][]string
+		Header http.Header
+	}{
+		req.Method,
+		req.URL,
+		req.Form,
+		req.Header,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+func main () {
+	var a airplane
+	http.ListenAndServe(":8080", a)
+}
